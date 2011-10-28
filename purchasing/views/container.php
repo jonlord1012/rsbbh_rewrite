da8f2130724a87b83c1568dee62ded5e63a6e054
Bayu Irawan <bayu.iravvan@gmail.com>
2011-10-29 04:53:44 +0700

    Initial purchase module

---
+++ purchasing/views/container.php
@@ -0,0 +1,5 @@
+<? $this->load->view('includes/header'); ?>
+<div class="main-content">
+  <?php $this->load->view($content);?>
+</div><!-- .main-content -->
+<?php $this->load->view('includes/footer');?>
---
+++ purchasing/views/menus/default.php
@@ -0,0 +1,15 @@
+<div id="main-nav" class="grid_12">
+  <div id="first-level">
+    <ul class="menu">
+      <li class="menu-item">
+        <a href="">List PO</a>
+      </li>
+      <li class="menu-item">
+        <a href="<?php echo site_url('purchase/po_baru');?>">PO Baru</a>
+      </li>
+      <li class="menu-item">
+        <a href="">Report</a>
+      </li>
+    </ul>
+  </div>
+</div>
