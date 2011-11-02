89d12f57b8d972cda6c09120f52b8f9957b3543e
Bayu Irawan <bayu.iravvan@gmail.com>
2011-11-03 02:16:45 +0700

    many changes

--- inventory/views/supplier/list.php
+++ inventory/views/supplier/list.php
@@ -1,9 +1,7 @@
 <div class="inner">
   <h3>Supplier List</h3>
   <div class="grid">
-    <?php if (empty($companies)): ?>
-    <h3>No factory</h3>
-    <?php endif;?>
-    <?php var_dump($companies);?>
+    <?php echo $js_grid;?>
+    <table id="supplier-grid"></table>
   </div><!-- .grid -->
 </div><!-- .inner  -->
